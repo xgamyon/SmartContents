7ee4f00f587f7060e0cb19aaec33f43678479dab
xgamyon <xgamyon@gmail.com>
2016-12-17 20:33:29 +0900

    20161217

--- jQuery/jQuery시험문제/js/script.js
+++ jQuery/jQuery시험문제/js/script.js
@@ -1,27 +1,75 @@
 $(function(){
     // Hover Menu
-        $('header ul>li').mouseenter(function(){
-            $(this).addClass('on').siblings().removeClass('on');
+        $('header ul>li+li').mouseenter(function(){
+            $(this).removeClass('out').addClass('on').siblings().removeClass('on');
         })
-        $('header ul>li').mouseleave(function(){
-            $(this).removeClass('on').addClass('out');
+        $('header ul>li+li').mouseleave(function(){
+            $(this).removeClass('on').addClass('out')
         })
 
 
-    // $('header ul>li').hover(function(){
-    //     var len = $('header ul>li').length;
-    //     var idx = $(this).index();
-    //
-    //     for(i=0; i<len; i++){
-    //         if(i == idx){
-    //             $('header ul>li').eq(i).removeClass('out').addClass('on');
-    //         }else{
-    //             $('header ul>li').eq(i).removeClass('on').addClass('out');
-    //         }
-    //     }
-    // })
+    // ScrollMove Menu
+        $('header ul>li').click(function(){
+            var idx = $(this).index();
+            // console.log(idx)
+            var top = $('.content section').eq(idx).offset().top;
+            // console.log(top)
+            $('body,html').animate({
+                'scrollTop':top
+            })
+
+        })
 
 
+
+    // Slide Banner
+        var wid = $('.slidebanner').width();
+        var idx = 0;
+
+        $('.next').click(function(){
+            $('.slidebanner ul li').eq(idx).find('img').animate({
+                'left':-wid
+            }).parent().next().find('img').css({
+                'display':'block',
+                'left':wid
+            }).stop().animate({
+                'left':0
+            })
+            idx ++
+            if(idx == $('.slidebanner ul li').length){
+                idx = 0
+                $('.slidebanner ul li').eq(idx).find('img').css({
+                    'left':wid
+                }).stop().animate({
+                    'left':0
+                })
+            }
+            //a 태그 이동
+            $('.slidebanner ul li').eq(idx).addClass('on').siblings().removeClass()
+        })
+
+        $('.prev').click(function(){
+            $('.slidebanner ul li').eq(idx).find('img').animate({
+                'left':wid
+            }).parent().prev().find('img').css({
+                'display':'block',
+                'left':-wid
+            }).stop().animate({
+                'left':0
+            })
+            idx --
+            if(idx < 0){
+                idx = $('.slidebanner ul li').length-1;
+                $('.slidebanner ul li').eq(idx).find('img').css({
+                    'display':'block',
+                    'left':-wid
+                }).stop().animate({
+                    'left':0
+                })
+            }
+            //a 태그 이동
+            $('.slidebanner ul li').eq(idx).addClass('on').siblings().removeClass()
+        })
 
 
 
