9fcb109c2a3de3741309f37f17abeb8334170d4c
xgamyon <xgamyon@gmail.com>
2016-12-27 18:30:01 +0900

    20161227

---
+++ java/Test.java
@@ -0,0 +1,14 @@
+
+class Test{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+ cd 하고 명령어로 java 위치까지 경로 들어가기
+ dir 은 파일 확인, D: 는 d 드라이브로 들어가기
+
+번역방법 : javac Test.javac
+실행방법 : java Test
+*/
