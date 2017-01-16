abb187bd9e9b3f28c356144115929e0404be1b48
xgamyon <xgamyon@gmail.com>
2017-01-16 18:17:12 +0900

    20170116

---
+++ java/workspace_20170116/Woorizip/js/common.js
@@ -0,0 +1,10 @@
+// 파일명 : common.js
+// 전화번호 : 입력형식이 맞는지체크한다.
+// 사용법 : if(isPhoneValid(frm.phone.value)==false){ ... }
+
+function isPhoneValid(object){
+	// 전화번호 입력형식이 맞는지 체크한다.
+	var reg = /(01[0|1|6|9|7])[-](\d{3}|\d{4})[-](\d{4}$)/g;
+	var result = reg.test(object);  // test 는 내장함수
+	return result; // 정상적일때
+}
